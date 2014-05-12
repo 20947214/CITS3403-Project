9b3513bb0bd8f5cd5af9b207952fabbbb6b3cbc9
Soju <20722145@student.uwa.edu.au>
2014-05-12 15:32:23 +0800

    initial file structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CITS3403Project::Application.config.secret_key_base = '3e24ed5c420b52fa84cea1acb3047a359d819072ce98b955181ab9467216aae77db18689b1134bf90378e7f287e6c6670ff8398518b13f7154d47d45d1d88dae'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CITS3403Project::Application.config.session_store :cookie_store, key: '_CITS3403-Project_session'
